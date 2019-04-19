ff9bebb733d20a3c6e58c597630b9cfeda422134
Roy Shen <roythroyalboy@gmail.com>
2019-04-19 11:55:22 -0400

    Time change and other adjustments

--- EKG_simulator.cpp
+++ EKG_simulator.cpp
@@ -3,30 +3,31 @@
 
 using namespace std;
 
+void simulate(EKG EKG_machine, int value){
+	EKG_object measurement;
+	//subtract the difference from 1/1/2000 to 1/1/1970 to get time from 1/1/2000
+	measurement.time = time(NULL) - 946684800;
+	measurement.ST_segment = value;
+	cout << "Time: " << measurement.time  << " | " << "ST_segment: " << measurement.ST_segment << endl;
+	EKG_machine.ProcessEKG(measurement);
+}
+
 int main(){
 	cout << "Welcome to EKG simulator!" << endl;
-	cout << "To simulate healthy heartbeat, type 'normal'" << endl;
-	cout << "To simulate unhealth heartbeat, type 'abnormal'" << endl;
+	cout << "To simulate healthy ST segments, type 'normal'" << endl;
+	cout << "To simulate unhealthy ST segments, type 'abnormal'" << endl;
 	cout << "Type ctrl-c to stop this simulator" << endl;
 	string mode;
 	cin >> mode;
 	EKG EKG_machine;
 	while (true) {
-		//pretend 2000 = 1970
 		if (mode == "normal"){
-			EKG_object measurement;
-			measurement.time = time(NULL);
-			measurement.ST_segment = rand() % 3 + 79;
-			cout << "Time: " << measurement.time << " | " << "ST_segment: " << measurement.ST_segment << endl;
-			EKG_machine.ProcessEKG(measurement);
+			//normal heartrates
+			simulate(EKG_machine, rand() % 3 + 79);
 		}
 		else if (mode == "abnormal") {
 			//abnormal ST segment, should print alerts
-			EKG_object measurement;
-			measurement.time = time(NULL);
-			measurement.ST_segment = rand() % 6 + 78;
-			cout << "Time: " << measurement.time  << " | " << "ST_segment: " << measurement.ST_segment << endl;
-			EKG_machine.ProcessEKG(measurement);
+			simulate(EKG_machine, rand() % 5 + 78);
 		}
 		else {
 			cout << "invalid command, exiting EKG simulator..." << endl;
